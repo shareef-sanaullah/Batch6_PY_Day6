cf85905196b443cf02b3ae9563c0fbe39c6d27a1
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-19 11:10:19 +0300

    Add files via upload

---
+++ Batch6_PY_Day6.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+dimensions = (250,50)
+print(dimensions)
+
+
+# In[2]:
+
+
+type(dimensions)
+
+
+# In[3]:
+
+
+dimensions = (200,50)
+print(dimensions)
+
+
+# In[4]:
+
+
+alien = {'color':'green','points':5}
+print(alien)
+
+
+# In[5]:
+
+
+type(alien)
+
+
+# In[6]:
+
+
+import this
+
+
+# In[ ]:
+
+
+
+
